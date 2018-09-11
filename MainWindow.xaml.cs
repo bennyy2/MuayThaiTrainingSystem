41e7d85d4bc2a4dc698a62e54cf3d3ca55e6dc8c
Benjarat Chantaprasert <sakjura@gmail.com>
2018-09-11 16:13:03 +0700

    get joint position

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -51,6 +51,7 @@ namespace MuayThaiTraining
                     kSensor.Start();
                     this.lbKinectID.Content = kSensor.DeviceConnectionId;
                     kSensor.ColorStream.Enable();
+                    kSensor.DepthStream.Enable();
                     kSensor.SkeletonStream.Enable();
                     kSensor.AllFramesReady += KSensor_AllFramesReady;
                 }
@@ -84,6 +85,8 @@ namespace MuayThaiTraining
 
         private void KSensor_AllFramesReady(object sender, AllFramesReadyEventArgs e)
         {
+
+            //using (DepthImageFrame colorFrame = e.OpenDepthImageFrame())
             using (ColorImageFrame colorFrame = e.OpenColorImageFrame())
             {
                 if (colorFrame == null)
@@ -92,6 +95,7 @@ namespace MuayThaiTraining
                 }
 
 
+                //byte[] colorData = GenerateColorByte(colorFrame);
                 byte[] colorData = new byte[colorFrame.PixelDataLength];
 
                 colorFrame.CopyPixelDataTo(colorData);
@@ -159,29 +163,79 @@ namespace MuayThaiTraining
                     DrawBone(skeleton, JointType.KneeRight, JointType.AnkleRight);
                     DrawBone(skeleton, JointType.AnkleRight, JointType.FootRight);
 
+                    Console.Write("X: " + skeleton.Joints[JointType.Spine].Position.X);
+                    Console.Write(" Y: " + skeleton.Joints[JointType.Spine].Position.Y);
+                    Console.Write(" Z: " + skeleton.Joints[JointType.Spine].Position.Z);
+                    Console.WriteLine(" ");
 
-                    //Console.Write("X: " + headPoint.X);
-                    //Console.Write(" Y: " + headPoint.Y);
-                    //Console.WriteLine(" ");
                 }
 
 
             }
         }
 
+        private byte[] GenerateColorByte(DepthImageFrame colorFrame)
+        {
+            short[] rawDepthData = new short[colorFrame.PixelDataLength];
+            colorFrame.CopyPixelDataTo(rawDepthData);
+
+            Byte[] pixels = new byte[colorFrame.Height * colorFrame.Width * 4];
+
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+
+            for (int depthIndex = 0, colorIndex = 0;
+                depthIndex < rawDepthData.Length && colorIndex < pixels.Length; 
+                depthIndex++, colorIndex += 4)
+            {
+                int player = rawDepthData[depthIndex] & DepthImageFrame.PlayerIndexBitmask;
+
+                int depth = rawDepthData[depthIndex] >> DepthImageFrame.PlayerIndexBitmaskWidth;
+
+                if (depth <= 900)
+                {
+                    pixels[colorIndex + BlueIndex] = 255;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                else if (depth > 900 && depth < 2000)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 255;
+                    pixels[colorIndex + RedIndex] = 0;
+                }
+                else if (depth > 2000)
+                {
+                    pixels[colorIndex + BlueIndex] = 0;
+                    pixels[colorIndex + GreenIndex] = 0;
+                    pixels[colorIndex + RedIndex] = 255;
+                }
+
+            }
+
+            return pixels;
+        }
+
+
         private void DrawBone(Skeleton skeleton, JointType joint1, JointType joint2)
         {
+            //DepthImagePoint joint1Point = kSensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skeleton.Joints[joint1].Position, DepthImageFormat.Resolution320x240Fps30);
+            //DepthImagePoint joint2Point = kSensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skeleton.Joints[joint2].Position, DepthImageFormat.Resolution320x240Fps30);
+
             ColorImagePoint joint1Point = kSensor.CoordinateMapper.MapSkeletonPointToColorPoint(skeleton.Joints[joint1].Position, ColorImageFormat.RgbResolution640x480Fps30);
             ColorImagePoint joint2Point = kSensor.CoordinateMapper.MapSkeletonPointToColorPoint(skeleton.Joints[joint2].Position, ColorImageFormat.RgbResolution640x480Fps30);
 
             Line backBone = new Line();
-            backBone.Stroke = new SolidColorBrush(Colors.Red);
+            backBone.Stroke = new SolidColorBrush(Colors.Yellow);
             backBone.StrokeThickness = 5;
 
             backBone.X1 = joint1Point.X;
             backBone.Y1 = joint1Point.Y;
             backBone.X2 = joint2Point.X;
             backBone.Y2 = joint2Point.Y;
+
+            
 
             skelCanvas.Children.Add(backBone);
         }
